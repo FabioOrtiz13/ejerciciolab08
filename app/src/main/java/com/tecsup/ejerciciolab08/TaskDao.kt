f322b961847578fe3efd8ca8316d1d6321e834c7
Fabio O <fortiz@unsa.edu.pe>
2025-05-13 20:57:07 -0500

    Paso 3: Creamos el DAO (Data Access Object)

---
+++ app/src/main/java/com/tecsup/ejerciciolab08/TaskDao.kt
@@ -0,0 +1,29 @@
+package com.tecsup.ejerciciolab08
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM tasks WHERE description LIKE :query")
+    suspend fun searchTasks(query: String): List<Task>
+
+    @Query("SELECT * FROM tasks")
+    suspend fun getAllTasks(): List<Task>
+
+    @Insert
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("DELETE FROM tasks")
+    suspend fun deleteAllTasks()
+}
